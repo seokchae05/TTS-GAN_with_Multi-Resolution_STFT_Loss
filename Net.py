d18e261ac4a3239ccaeb7f0057b3d07089091c1a
Hoijun Jeoung <hoijunjeoung@Hoijunui-MacBookPro.local>
2023-01-10 21:56:36 +0900

    code more added

--- Net.py
+++ Net.py
@@ -16,7 +16,6 @@ def weights_init(mod):
         mod.weight.data.normal_(1.0, 0.02)
         mod.bias.data.fill_(0)
 
-###
 class Encoder(nn.Module):
     """
     DCGAN ENCODER NETWORK
@@ -41,7 +40,7 @@ class Encoder(nn.Module):
         main = nn.Sequential()
         # input is nc x isize x isize
         main.add_module('initial-conv-{0}-{1}'.format(nc, ndf),
-                        nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
+                        nn.Conv2d(nc, ndf, 3, 2, 1, bias=False))
         main.add_module('initial-relu-{0}'.format(ndf),
                         nn.LeakyReLU(0.2, inplace=True))
         csize, cndf = isize / 2, ndf
@@ -59,7 +58,7 @@ class Encoder(nn.Module):
             in_feat = cndf
             out_feat = cndf * 2
             main.add_module('pyramid-{0}-{1}-conv'.format(in_feat, out_feat),
-                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
+                            nn.Conv2d(in_feat, out_feat, 3, 2, 1, bias=False))
             main.add_module('pyramid-{0}-batchnorm'.format(out_feat),
                             nn.BatchNorm2d(out_feat))
             main.add_module('pyramid-{0}-relu'.format(out_feat),
@@ -70,19 +69,19 @@ class Encoder(nn.Module):
         # state size. K x 4 x 4
         if add_final_conv:
             main.add_module('final-{0}-{1}-conv'.format(cndf, 1),
-                            nn.Conv2d(cndf, nz, 4, 1, 0, bias=False))
+                            nn.Conv2d(cndf, nz, 3, 1, 0, bias=False))
 
         self.main = main
 
     def forward(self, input):
-        if self.ngpu > 1:
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else:
-            output = self.main(input)
+        with torch.autograd.set_detect_anomaly(True):
+            if self.ngpu > 1:
+                output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+            else:
+                output = self.main(input)
 
-        return output
-
-##
+            return output
+    
 class Decoder(nn.Module):
     """
     DCGAN DECODER NETWORK
@@ -101,8 +100,9 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
-
+        
         cngf, tisize = ngf // 2, 4
+        
         while tisize != isize:
             cngf = cngf * 2
             tisize = tisize * 2
@@ -110,7 +110,7 @@ class Decoder(nn.Module):
         main = nn.Sequential()
         # input is Z, going into a convolution
         main.add_module('initial-{0}-{1}-convt'.format(nz, cngf),
-                        nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
+                        nn.ConvTranspose2d(nz, cngf, 3, 1, 0, bias=False))
         main.add_module('initial-{0}-batchnorm'.format(cngf),
                         nn.BatchNorm2d(cngf))
         main.add_module('initial-{0}-relu'.format(cngf),
@@ -143,10 +143,122 @@ class Decoder(nn.Module):
         self.main = main
 
     def forward(self, input):
-        if self.ngpu > 1:
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else:
-            output = self.main(input)
-        return output
+        with torch.autograd.set_detect_anomaly(True):
+            if self.ngpu > 1:
+                output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+            else:
+                output = self.main(input)
+            return output
     
     
+    
+class Generator(nn.Module):
+    def __init__(self, n_cls, isize, nz, nc, ngf, ngpu, n_extra_layers=0):
+        super(Generator, self).__init__()
+        self.n_cls = n_cls
+        self.pre_z = nn.Sequential(
+            nn.Linear(2048, 3*nz),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.pre_c = nn.Sequential(
+            nn.Linear(n_cls, 1*nz),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layers = Decoder(isize=isize, nz=nz, nc=nc, ngf=ngf, ngpu=ngpu, n_extra_layers=n_extra_layers)
+        
+    def forward(self, z, c):
+        with torch.autograd.set_detect_anomaly(True):
+            batch_size = z.size(0)
+            
+            z = z.view([batch_size, -1])
+            z = self.pre_z(z)
+            z = z.view([batch_size, -1, 2, 2])
+            c = self.pre_c(c)
+            c = c.view([batch_size, -1, 2, 2])
+            
+            zc = torch.cat([z,c], dim=1)
+
+            return self.layers(zc)
+
+
+class Discriminator(nn.Module):
+    def __init__(self,n_cls=2, isize=512, nz=100, nc=4, ngf=64, ngpu=0):
+        super(Discriminator, self).__init__()
+        
+        self.pre_x = Encoder(isize=512, nz=100, nc=4, ndf=64, ngpu=0)
+        # 2x2x100
+        
+        self.pre_z = nn.Sequential(
+            nn.Linear(2048, 3*nz),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        # flatten(2x2x75)
+        
+        
+        self.pre_c = nn.Sequential(
+            nn.Linear(n_cls, 1*nz),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        # flatten(2x2x25)
+        
+        self.layers = nn.Sequential(
+            nn.Linear(2448, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            
+        
+            nn.Linear(1024, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+            
+
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+        
+
+        
+    def forward(self, x, z, c):
+        with torch.autograd.set_detect_anomaly(True):
+            batch_size = x.size(0)
+            
+            x = self.pre_x(x)
+            z = z.view(batch_size, -1)
+
+            z = self.pre_z(z)
+            
+            
+            
+            z = z.view([batch_size, -1, 2, 2])
+            c = self.pre_c(c)
+            c = c.view([batch_size, -1, 2, 2])
+            
+            zc = torch.cat([z,c], dim=1)
+            xzc = torch.cat([x,zc], dim=1)
+            
+            # 2x2x200
+            xzc = xzc.view(batch_size, -1)
+            
+            result = self.layers(xzc)
+            
+        
+            return result
+
+def D_loss(DG, DE, eps=1e-6):
+    loss = torch.log(DE + eps) + torch.log(1 - DG + eps)
+    return -torch.mean(loss)
+
+
+def EG_loss(DG, DE, eps=1e-6):
+    loss = torch.log(DG + eps) + torch.log(1 - DE + eps)
+    return -torch.mean(loss)
+
+def initialize_weights(model):
+    classname = model.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(model.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(model.weight.data, 1.0, 0.02)
+        nn.init.constant_(model.bias.data,0)
+    elif classname.find('Linear') != -1:
+        nn.init.constant_(model.bias.data, 0)
---
+++ Testing.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+
+import Utils
+import os
+
+from Net import Encoder, Generator, Discriminator, D_loss, EG_loss, initialize_weights
+
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    n_epochs = 500
+    l_rate = 2e-5
+
+    print('Model Creating...')
+    E = Encoder(isize=512, nz=512, nc=4, ndf=64, ngpu=0)
+    G = Generator(n_cls=2, isize=512, nz=512, nc=4, ngf=64, ngpu=0)
+    D = Discriminator(n_cls=2, isize=512, nz=512, nc=4, ngf=64, ngpu=0)
+
+    E.apply(initialize_weights)
+    G.apply(initialize_weights)
+    D.apply(initialize_weights)
+
+    optimizer_DEG = torch.optim.Adam(list(E.parameters()) + list(G.parameters()) + list(D.parameters()), 
+                                    lr=l_rate, betas=(0.5, 0.999), weight_decay=1e-5)
+
+    print(f'Data loading...')
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(size=[512,512]),
+        transforms.Normalize(mean=(0.5,), std=(0.5, ))
+    ])
+
+    root_path = os.path.join(os.getcwd(), 'data')
+    dataset = Utils.vibrationData(root_path=root_path, transform=transform)
+
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True)
+
+
+    print('Start learning...')
+    device = 'cpu'
+    for epoch in range(n_epochs):
+        D_loss_acc = 0.
+        EG_loss_acc = 0.
+        D.train()
+        E.train()
+        G.train()
+        
+        for i, (signal, images, corr_img, c) in tqdm(enumerate(data_loader)):
+            
+            images = images.to(device)
+            
+            rand_z = 2 * torch.rand(images.size(0), 512, 2, 2) - 1
+            rand_z = rand_z.to(device)
+            
+            #compute G(z, c) and E(X)
+            
+            Gz = G(rand_z, c)
+            EX = E(images)
+            
+            # print(f'image : {images.size()}, Gz : {Gz.size()}')
+            # print(f'rand_z : {rand_z.size()}, Ex : {EX.size()}')
+            DG = D(Gz, rand_z, c)
+            DE = D(images, EX, c)
+            
+            #compute losses
+            loss_D = D_loss(DG, DE)
+            loss_EG = EG_loss(DG, DE)
+            D_loss_acc += loss_D.item()
+            EG_loss_acc += loss_EG.item()
+            
+            loss_DEG= loss_D +loss_EG
+            
+            optimizer_DEG.zero_grad()
+            loss_DEG.backward()
+            optimizer_DEG.step()
+            
+            if (epoch + 1) % 10 == 0:
+                print('Epoch [{}/{}], Avg_Loss_D: {:.4f}, Avg_Loss_EG: {:.4f}'
+                        .format(epoch + 1, n_epochs, D_loss_acc / i, EG_loss_acc / i))
+                n_show = 10
